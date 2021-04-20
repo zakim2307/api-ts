dcdeddc036b56acf8f54f9937d845edb744f9dfb
zakim2307 <zakim2307@gmail.com>
2021-04-20 12:56:49 +0530

    post issues resoleved

--- lambdas/postCityInfo.ts
+++ lambdas/postCityInfo.ts
@@ -5,22 +5,16 @@ import { Inventory } from './interfaces/inventory';
 import schema from './models/schema'
 
 export const handler: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event, _context) => {
-  const name = event.pathParameters?.name;
+  const body1 = JSON.parse(event.body); 
 
-  console.log('name is:',name)
-  if(!name){
-    //   return apiResponses._400({message: 'Cannot find name'})
+  if(!body1){
+      return apiResponses._400({message: 'Cannot find name'})
     }
-    
-    const nm:string = name;
-    let newIn: { [key: string]:Inventory };
-    let d1 = new Date();
-    console.log(Date.now())
-    // const sql:string = `INSERT INTO "Inventory" ( "isActive", "createDateTime", "createdBy", "lastChangedDateTime", "lastChangedBy", name, description) VALUES (true, to_timestamp(${Date.now()} / 1000.0), \'Admin\', \'2021-04-12 10:57:50.134+05:30\', \'Admin\', \'Sand\', \'large grained\');`;
-    // const { rows } = await pool.query(sql);
-    // console.log(rows);
+    const sql:string = `INSERT INTO "Inventory" ( "isActive", "createDateTime", "createdBy", "lastChangedDateTime", "lastChangedBy", name, description) VALUES (true, to_timestamp(${body1["createDateTime"]} / 1000.0), \'${body1["createdBy"]}\', to_timestamp(${body1["lastChangedDateTime"]} / 1000.0), \'${body1["lastChangedBy"]}\', '${body1['name']}', \'${body1['description']}\');`;
+    const { rows } = await pool.query(sql);
+    console.log(rows);
 
-    return apiResponses._200(name);
+    return apiResponses._200(body1["createDateTime"]);
 };
     const apiResponses = {
         _200: (body: any) => {
